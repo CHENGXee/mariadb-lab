86eccb1b1d1aea054ce815fbbd454c31fabe007b
CHENGXee <411630394@o365.tku.edu.tw>
2025-03-11 00:36:00 +0800

    建立interface

---
+++ Frontend/src/interface/Reservaiton.ts
@@ -0,0 +1,11 @@
+import { Student } from "./Student";
+import { Seat } from "./Seat";
+import { Timeslot } from "./Timeslots";
+
+export interface Reservations {
+    reservation_id: number;
+    student: Student;
+    seat: Seat;
+    timeslot: Timeslot;
+    create_time: string;
+}
