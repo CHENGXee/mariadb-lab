c3935f07adfe9f10003f1f923aa710dfac7b494c
CHENGXee <411630394@o365.tku.edu.tw>
2025-03-11 00:36:27 +0800

    實現透過api請求Reservations資料

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { asyncGet } from '../utils/fetch';
+import { reservations_api } from './enum/api';
+import { Reservations } from './interface/Reservaiton';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [reservations, setReservations] = useState([]);
+  const fetchReservation = async () => {
+    try {
+      const response = await asyncGet(reservations_api.list);
+      setReservations(response.reservations)
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
+  useEffect(() => {
+    fetchReservation();
+  }, [])  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <h1>Reservation List</h1>
+      <div className="grid-container">
+        {reservations.map((res: Reservations, index) => (
+          <div key={index} className="card">
+            <p className="id">ID: {res.reservation_id}</p>
+            <h2>Seat: {res.seat.row_label}-{res.seat.seat_number}</h2>
+            <p className="student">
+              🎓 <strong>{res.student.student_name}</strong> (ID: {res.student.student_id})
+            </p>
+            <p className="time">
+              🕒 {res.timeslot.start_time} - {res.timeslot.end_time}
+            </p>
+            <p className="date">📅 {new Date(res.create_time).toLocaleString()}</p>
+          </div>
+        ))}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
 export default App
